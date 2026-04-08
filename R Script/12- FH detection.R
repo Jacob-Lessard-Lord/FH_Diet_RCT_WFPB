8754d5bcd2615d071bf5de3a19b5774c133fec48
Jacob-Lessard-Lord <79337010+Jacob-Lessard-Lord@users.noreply.github.com>
2026-04-08 09:48:36 -0400

    Update 12- FH detection.R

--- R Script/12- FH detection.R
+++ R Script/12- FH detection.R
@@ -29,15 +29,3 @@ stat_test <- stat_test %>%
 
 # Creation d'un tableau de frequence
 df_freq <- data_post_pp %>% freq_table(tx, fh_detection)
-
-# Graphique
-setwd("C:/Users/jackl/OneDrive/Bureau/École/Postdoctorat/Chercheur/Jean-Philippe/FH-Diet/Processing_FH-Diet/Data")
-
-tiff(filename = "Figure S2.tiff", width = 5.75, height = 12, units = "cm", res = 600, compression = "lzw")
-ggbarplot(data = subset(df_freq, subset = fh_detection == "FH detected"),
-          x = "tx", y = "prop", fill = "tx",
-          ylab = "Proportion of FH-detected subjects (%)",
-          label = TRUE, palette = c("#df5f01", "#0160cd"), legend = "none")+ 
-  scale_x_discrete(labels = c("SAD", "WFPB"), name = NULL) + 
-  stat_pvalue_manual(stat_test, label = "P = {p}", tip.length = 0.15)
-dev.off()
